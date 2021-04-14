3af5e22baaa7c9293cf2f2bbf2355c2e22c7ba4b
nandan kumar <nandankmrjha@gmail.com>
2021-04-15 01:26:17 +0530

    show user data

--- pages/eom.js
+++
@@ -1,9 +0,0 @@
-const EOM = () => {
-  return (
-    <div className="page-container">
-      <h1>Employee of the Month</h1>
-    </div>
-  );
-};
-
-export default EOM;
---
+++ pages/users.js
@@ -0,0 +1,37 @@
+import styles from "../styles/UserList.module.css"
+
+const UserList = ({ users }) => {
+  console.log(users);
+  return (
+    <div className="page-container">
+      <h1>Employee of the Month</h1>
+      <div className={styles.main}>
+        <img src={users.data[0].picture}></img>
+        <span className={styles.userData}>
+          <span>name: {users.data[0].firstName}</span>
+          <span>email: {users.data[0].email}</span>
+        </span>
+      </div>
+    </div>
+  );
+};
+
+export const getServerSideProps = async (pageContext) => {
+  const endpoint = "https://dummyapi.io/data/api/";
+  const appId = "60773cfad4aae5505416b4a0";
+  const dataSet = await fetch(endpoint + "user", {
+    headers: {
+      "app-id": appId,
+    },
+  });
+
+  const users = await dataSet.json();
+
+  return {
+    props: {
+      users,
+    },
+  };
+};
+
+export default UserList;
